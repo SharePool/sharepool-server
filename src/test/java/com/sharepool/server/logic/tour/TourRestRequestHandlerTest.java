56ca7a8a0a6a8947d5f102b64cfdb0c46d367593
jwi <jan.wiesbauer@icloud.com>
2019-05-28 21:03:10 +0200

    Add transitional annotation to failed Tests

--- src/test/java/com/sharepool/server/logic/tour/TourRestRequestHandlerTest.java
+++ src/test/java/com/sharepool/server/logic/tour/TourRestRequestHandlerTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Currency;
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class TourRestRequestHandlerTest {
 
     @Autowired
