3ab03d287c02a4628e71c481b7bea01b62d0c810
jwi <jan.wiesbauer@icloud.com>
2019-06-24 20:42:30 +0200

    Simplify RabbitMQ listener

--- sharepool-analytics/src/main/java/com/shareppol/sharepoolanalytics/SharePoolAnalyticsApplication.java
+++ sharepool-analytics/src/main/java/com/shareppol/sharepoolanalytics/SharePoolAnalyticsApplication.java
@@ -1,57 +1,15 @@
 package com.shareppol.sharepoolanalytics;
 
-import com.shareppol.sharepoolanalytics.rabbitmq.Receiver;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class SharePoolAnalyticsApplication {
 
-    static final String topicExchangeName = "sharepool-exchange";
-    static final String queueName = "sharepool-analytics";
-
     public static void main(String[] args) {
         SpringApplication.run(SharePoolAnalyticsApplication.class, args);
-    }
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
-    }
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(topicExchangeName);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("#");
-    }
-
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(queueName);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
     }
 
 }
--- sharepool-analytics/src/main/java/com/shareppol/sharepoolanalytics/rabbitmq/Receiver.java
+++ sharepool-analytics/src/main/java/com/shareppol/sharepoolanalytics/rabbitmq/Receiver.java
@@ -2,6 +2,7 @@ package com.shareppol.sharepoolanalytics.rabbitmq;
 
 import com.shareppol.sharepoolanalytics.domain.AnalyticsMessage;
 import com.shareppol.sharepoolanalytics.logic.AnalyticsMessageRabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +14,7 @@ public class Receiver {
         this.requestHandler = requestHandler;
     }
 
+    @RabbitListener(queues = {"sharepool-analytics"})
     public void receiveMessage(AnalyticsMessage analyticsMessage) {
         System.out.println("Received <" + analyticsMessage.toString() + ">");
 
