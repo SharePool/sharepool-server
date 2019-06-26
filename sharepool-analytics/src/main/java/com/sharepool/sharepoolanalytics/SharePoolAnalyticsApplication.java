96ae7ad35c8fd61416e15ea4eac506e7d4af31da
Tobias Kaderle <kaderle.tobias@hotmail.com>
2019-06-26 10:11:03 +0200

    Enable swagger

--- sharepool-analytics/src/main/java/com/sharepool/sharepoolanalytics/SharePoolAnalyticsApplication.java
+++ sharepool-analytics/src/main/java/com/sharepool/sharepoolanalytics/SharePoolAnalyticsApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSwagger2
 public class SharePoolAnalyticsApplication {
 
     public static void main(String[] args) {
